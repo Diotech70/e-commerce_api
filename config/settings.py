78aa9136708f9de877dc212b61a7748405587193
Diotech70 <adebayohammed70@gmail.com>
2025-11-30 17:51:10 +0100

    added requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,10 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 
 STATICFILES_STORAGE ='whitenoise.storge.CompressManifestStaticFilesStorage'
 
+MEDIA_URL ='/media/'
+
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
