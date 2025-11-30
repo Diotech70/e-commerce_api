c3e507e30853e4393a0edd0aa5136d1c6502778c
Diotech70 <adebayohammed70@gmail.com>
2025-11-30 16:46:36 +0100

    add .gitignore properly

--- carts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import CartListView, AddToCartView, UpdateCartView, RemoveCartView
-
-urlpatterns =[path('',CartListView.as_view(),name='cart-list'),
-               path('add/',AddToCartView.as_view(),name='add-cart'),
-               path('update/<int:pk>/',UpdateCartView.as_view(),name='cart-update'),
-               path('remove/<int:pk>/',RemoveCartView.as_view(),name='cart-remove'),
-]
--- order/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import OrderListView, CreateOrderView, OrderPayView
-
-urlpatterns =[path('create/',CreateOrderView.as_view(),name='create-order'),
-              path('',OrderListView.as_view(),name='list-order'),
-              path('<int:pk>/pay/',OrderPayView.as_view(),name='pay-order'),
-]
