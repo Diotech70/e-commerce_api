0b8971fab484659c70585582364879a8c921cfe8
Diotech70 <adebayohammed70@gmail.com>
2025-11-30 18:21:01 +0100

    databse-url settings

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,7 @@ SWAGGER_SETTINGS ={'SECURITY_DEFINITIONS':{'Bearer':{'type':'apiKey',
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-   DATABASES ={'default':dj_database_url.parse(DATABASE_URL=conn_max_age=600,ssl_require=True,),}
+   DATABASES ={'default':dj_database_url.parse(DATABASE_URL,conn_max_age=600,ssl_require=True,),}
 else:
     DATABASES ={'default':{'ENGINE':'django.db.backends.sqlite3',
                 'NAME': BASE_DIR/'db.sqlite3',},}
